5b46fd89163b5f32b7f4083630a020ba50125a11
Villani-tc <tiagov.ufabc@gmail.com>
2022-11-26 01:39:00 -0300

    Criação do Scraping do site Kabum, e correção doML

---
+++ Kabum.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+def Busca_Kabum(produto_nome):
+    base_url = 'https://www.kabum.com.br'
+    url = 'https://www.kabum.com.br/busca/'
+    url = url + produto_nome
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+    site = BeautifulSoup(response.text, 'html.parser')
+    produtos = site.findAll('div', attrs={'class':'sc-ff8a9791-7 dZlrn productCard'})
+
+    for produto in produtos:
+
+        produto_titulo = produto.find('span', attrs= {'class':'sc-d99ca57-0 iRparH sc-ff8a9791-16 kRYNji nameCard'}).text
+        produto_preco = produto.find('span', attrs={'class': 'sc-3b515ca1-2 jTvomc priceCard'}).text
+        produto_link = produto.find('a', attrs= {'class':'sc-ff8a9791-10 cUkkYl'})
+        produto_link = str(base_url + str(produto_link['href']))
+
+        print('Título do Produto: ' + produto_titulo )
+        print('Preço do Produto: ' + produto_preco )
+        print('Link: ' + produto_link )
+        print('\n')
+
--- app.py
+++ app.py
@@ -1,52 +1,7 @@
-import requests
-from bs4 import BeautifulSoup
+from ML import Busca_ML
+from Kabum import Busca_Kabum
 
-
-url = 'https://lista.mercadolivre.com.br/'
-produto_nome = 'mi band 7'
-response = requests.get(url+produto_nome)
-
-site = BeautifulSoup(response.text, 'html.parser')
-
-produtos = site.findAll('div', attrs={'class':'andes-card andes-card--flat andes-card--default ui-search-result shops__cardStyles ui-search-result--core andes-card--padding-default'})
-
-for produto in produtos:
-
-    produto_titulo = produto.find('h2', attrs = {'class': 'ui-search-item__title shops__item-title'}).text
-    produto_preco = produto.find('span', attrs={'class':'price-tag-fraction'}).text    
-    produto_preco_cents = produto.find('span', attrs={'class':'price-tag-cents'})
-    produto_preco_cents = str(produto_preco_cents)
-    produto_preco_cents = produto_preco_cents[30:32]
-    final_preco = produto_preco + ','+ produto_preco_cents
-    link = produto.find('a',attrs={'class':'ui-search-link'})
-    link = link['href']
-    parcelas = produto.find('span', attrs={'class': 'ui-search-item__group__element shops__items-group-details ui-search-installments ui-search-color--BLACK'})
-    if (parcelas):
-        parcelas = produto.find('span', attrs={'class': 'ui-search-item__group__element shops__items-group-details ui-search-installments ui-search-color--BLACK'}).text
-        parcelas = str(parcelas)
-        parcelas =parcelas[2:5]
-        parcelas_valor = produto.find('div', attrs={'class': 'ui-search-price ui-search-price--size-x-tiny ui-search-color--BLACK shops__price'}).text
-        parcelas_valor = str(parcelas_valor)
-        parcelas_valor = parcelas_valor[25:]
-    else:
-        parcelas = None
-
-    print('Título do Produto: ' + produto_titulo )
-    if (produto_preco_cents):
-        print('Preço do Produto: ' + final_preco )
-    else:
-        print('Preço do Produto: ' + produto_preco + ',00')
-    
-    if parcelas == None:
-        print('Sem Parcelamento')
-        
-    else: 
-        print('Número de Parcelas ' + parcelas + ' No valor de ' + parcelas_valor)
-
-    print('Link: ' + link)
-    print('\n\n')
-    
-
-
-
-
+produto_nome = ''
+Busca_ML(produto_nome)
+print('-----------------------------------------------------------------------------------------------------------------------------------------------------------')
+Busca_Kabum(produto_nome)
