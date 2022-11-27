66b16d3d01cf0f0c17b3b93529141f805375f380
tiago.carvalho <tiago.carvalho@voxage.com.br>
2022-11-27 17:43:31 -0300

    Fase 1

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from ML import Busca_ML
 from Kabum import Busca_Kabum
 import random as rn
 import pandas as pd
+import glob 
+import csv
+
 produto_nome = 'RTX 2060'
 try:
     Busca_ML(produto_nome)
@@ -9,7 +12,13 @@ except:
     print('Scrap não disponível ainda para essa secção do ML')
 
 Busca_Kabum(produto_nome)
-first_csv = pd.read_csv('Busca_DUMP\kabum.csv',sep=';',encoding='UTF-8',names=['Titulo Produto', 'Preço do Produto', 'Link', 'Site'])
-second_csv = pd.read_csv('Busca_DUMP\mercadolivre.csv',sep=';',encoding='UTF-8',names=['Titulo Produto', 'Preço do Produto', 'Link', 'Site'])
-df = pd.concat([first_csv,second_csv])
+data = []
+init = pd.DataFrame(data=data, index=None, columns=['Titulo Produto', 'Preço do Produto', 'Link', 'Site'], dtype=None, copy=None)
+init.to_csv('init.csv',encoding='UTF-8')
+data_main  = glob.glob( str ( 'Busca_DUMP' ) +  '/*.csv' , recursive = True )
+init = pd.read_csv('init.csv')
+for data in data_main:
+        data_csv = pd.read_csv(data,sep=';',encoding='UTF-8',names=['Titulo Produto', 'Preço do Produto', 'Link', 'Site'])
+        df = pd.concat([data_csv,init])
+        print(df)
 df.to_csv('output.csv',sep=';',encoding='UTF-8')
